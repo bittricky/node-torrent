0f3e0e73e5fba94279a2e3721151d3789a93224c
Mitul Patel <me@mitulpa.tel>
2024-05-25 15:27:23 -0400

    fix(entry): fix the path to the puppy torrent file

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import torrentParser from "./src/torrent/torrent-parser";
 import { getPeers } from "./src/tracker/tracker";
 import { download } from "./src/client/peer";
 
-const torrentPath = process.argv[2];
+const torrentPath = "./test/puppy.torrent";
 const torrent = torrentParser.open(torrentPath);
 
 getPeers(torrent, (peers) => {
-  peers.forEach((peerAddress) => {
-    download(peerAddress, torrent);
+  peers.forEach((peer) => {
+    download(peer, torrent);
   });
 });
